24e626098682922fef4d29e083e2c09a21edfb93
awakenlyon <lyon.wario77@gmail.com>
2024-05-12 07:03:34 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,20 +9,28 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGl test", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGl Window", NULL, NULL);
 	if (window == NULL)
 	{
-		std::cout << "not working dude" << std::endl;
+		std::cout << "<Error> Failed To Display the window" << std::endl;
 		glfwTerminate();
 	}
 	glfwMakeContextCurrent(window);
 
-	while (!glfwWindowShouldClose(window)
+	gladLoadGL();
+	
+	glViewport(0, 0, 800, 800);
+
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
 	}
 
-	glfwDestroyWindow(window);
+	glfwDestroyWindow(window);	
 
 	glfwTerminate();
 	return 0;
